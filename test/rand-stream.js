ff008ff0c03e5aed2e939d24dc4d6a1788083ef0
gilmichael <bangonkali@gmail.com>
2017-03-21 14:53:03 +0800

    build (module): setup for typescript development
    - moved lib to src
    - modified project.json to output compiled script to built dir
    - installed typescript, and other deps
    - ensure test script still works when issuing comman npm test
    
    - fixes #1

--- test/rand-stream.js
+++ test/rand-stream.js
@@ -1,10 +1,11 @@
 /* Your tests will go here */
-var RandStream = require('../lib/lib').RandStream,
-  assert = require('assert');
+var RandStream = require('../built/lib/lib').RandStream,
+    assert = require('assert');
 
 describe('Sample RandStream test', function () {
-  it('should pass if stream is readable', function () {
-    var stream = new RandStream();
-    assert(stream.readable);
-  });
+    it('should pass if stream is readable', function () {
+        var stream = new RandStream();
+        assert(stream.readable);
+    });
 });
+
