08b97d58e9100d839b1b4d91a43b65d8d86b84ae
gilmichael <bangonkali@gmail.com>
2017-03-21 17:51:14 +0800

    test (asncop): adds an extended test.
    - test includes an array on beginning and end of data.
    
    - fixes #4

--- src/test/async-op.js
+++ src/test/async-op.js
@@ -49,5 +49,101 @@ describe('Cycle test for AsyncOp', function() {
         problemset.doAsyncWithInterface(data, asyncOp);
     });
 
+    it('extended test, array begin and array end.', function(done) {
+        /** Temporary storage for actual values. */
+        let actual = [];
+
+        /** Expected array output. */
+        let expected = [ 'START: U',
+            'START: V',
+            'START: W',
+            'FINISH: U',
+            'FINISH: V',
+            'FINISH: W',
+            'START: A',
+            'FINISH: A',
+            'START: B',
+            'START: C',
+            'START: E',
+            'START: F',
+            'START: G',
+            'START: H',
+            'FINISH: B',
+            'FINISH: C',
+            'FINISH: E',
+            'FINISH: F',
+            'FINISH: G',
+            'FINISH: H',
+            'START: I',
+            'START: J',
+            'START: K',
+            'FINISH: I',
+            'FINISH: J',
+            'FINISH: K',
+            'START: L',
+            'FINISH: L',
+            'START: M',
+            'FINISH: M',
+            'START: N',
+            'START: O',
+            'START: P',
+            'START: Q',
+            'FINISH: N',
+            'FINISH: O',
+            'FINISH: P',
+            'FINISH: Q',
+            'START: D',
+            'FINISH: D',
+            'START: R',
+            'START: S',
+            'START: T',
+            'FINISH: R',
+            'FINISH: S',
+            'FINISH: T' 
+        ];
+
+        /** Dummy asyncOp implementation that matches IAsyncOp interface.
+         * @param {*} input the input array.
+         * @param {*} callback callback for resolve.
+         */
+        let asyncOp = (input, callback) => {
+            actual.push(`START: ${input}`);
+
+            let prom = new Promise(function(resolve) {
+                setTimeout(() => {
+                    actual.push(`FINISH: ${input}`);
+                resolve();
+            }, 100);
+            });
+
+            if(!callback) {
+                return prom;
+            }
+
+            prom.then(callback);
+        };
+
+        /** Data for testing. */
+        let data = [
+            ['U', 'V', 'W'],
+            'A',
+            [ 'B', 'C', 'E', 'F', 'G', 'H' ],
+            [ 'I', 'J', 'K'],
+            'L',
+            'M',
+            [ 'N', 'O', 'P', 'Q'],
+            'D',
+            ['R', 'S', 'T']
+        ];
+
+        setTimeout(() => {
+            for (var i = 0; i < expected.length; i++ ){
+                assert.equal(expected[i], actual[i]);
+            }
+            done();
+        }, 1100);
+
+        problemset.doAsyncWithInterface(data, asyncOp);
+    });
 
 });
