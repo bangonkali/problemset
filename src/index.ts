fb3f40866682596504e42fb350aa25f82fbfe876
gilmichael <bangonkali@gmail.com>
2017-03-22 01:45:35 +0800

    fix (problemset): exporting common modules.
    - RandStream and asyncOp are now exposed.
    
    - Fixes #7

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
-/**
- * Solutions
- */
-
-/**
- * Exports
- */
+/** Exports */
 export { ResourceManager, Resource, ResourceCallback } from './solutions/ResourceManager';
 export { doAsync, doAsyncWithInterface } from './solutions/Async';
 export { RandStringSource } from './solutions/RandStringSource';
+export { RandStream, asyncOp } from './lib/lib';
